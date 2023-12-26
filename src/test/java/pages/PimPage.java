8929228c79b60430d81c7c1d021c30d9faeb9122
Riana Ardiansari <rianaardiansari3009@gmail.com>
2023-12-26 14:56:41 +0700

    update add employee link again

--- src/test/java/pages/PimPage.java
+++ src/test/java/pages/PimPage.java
@@ -29,6 +29,7 @@ public class PimPage {
     }
 
     public void userClickAddEmployeeButton(){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3)); //add delay
         driver.findElement(add_employee_link).click();
     }
 
